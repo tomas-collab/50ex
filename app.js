ba1804410886fd8df0b1ddfed5e4f969a5e0f512
tomas berhane <tomib@tomass-MacBook-Air.local>
2021-05-30 12:56:32 +0200

    solved all question up to 22

---
+++ app.js
@@ -0,0 +1,27 @@
+// 21) Given variable x = "John" and y = "Doe", write on the console log "John <> Doe"
+//         22) Create an object with properties such name, surname, email
+//         23) Delete Email from the previously created object
+//         24) Create an array with 10 strings in it
+//         25) Print in the console every string in the previous array
+//         26) Create an array with 100 random numbers in it
+//         27) Wrote a function to get the MAX and the MIN from the previously created array
+//         28) Create an array of arrays, in which every array has 10 random numbers
+//         29) Create a function that gets 2 arrays and returns the longest one
+//         30) Create a function that gets 2 arrays of numbers and returns the one with the higher sum of values
+
+
+
+
+
+x = 'John'
+y = 'Doe'
+console.log(`${x} <> ${y}`) 
+
+
+let person= {
+    name:'John',
+    surname:'Doe',
+    email: 'Johndoe@gmail.com'
+}
+console.log(person)
+
